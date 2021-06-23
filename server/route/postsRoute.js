aed4c60bd9bc473b8683eabf582daa80315d4142
rico3d2001 <rico3d2001@gmail.com>
2021-06-23 07:46:52 -0300

    Inicio

---
+++ server/route/postsRoute.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/posts', async function(req,res){
+     res.json([{
+          id: 1,
+          title: 'REST API: Seidor Teste',
+          content: '...',
+          date: new Date()
+
+     }]);
+});
+router.get('/posts/:id', async function(req,res){
+    
+});
+router.post('/posts', async function(req,res){
+    
+});
+router.put('/posts/:id', async function(req,res){
+    
+});
+router.delete('/posts/:id', async function(req,res){
+    
+});
+
+module.exports = router;
